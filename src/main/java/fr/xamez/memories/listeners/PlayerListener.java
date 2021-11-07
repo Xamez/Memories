2dba57eb0532e669d40015fda92ccf47393fcb21
Xamez <34zemax34@gmail.com>
2021-11-07 16:35:46 +0100

    ✨ Added a way to destroy block

--- src/main/java/fr/xamez/memories/listeners/PlayerListener.java
+++ src/main/java/fr/xamez/memories/listeners/PlayerListener.java
@@ -2,28 +2,38 @@ package fr.xamez.memories.listeners;
 
 import fr.mrmicky.fastboard.FastBoard;
 import fr.xamez.memories.Memories;
-import fr.xamez.memories.game.GameClock;
-import fr.xamez.memories.struct.AbstractStruct;
 import fr.xamez.memories.commands.MemoriesCMD;
 import fr.xamez.memories.runnables.ScoreboardRunnable;
+import fr.xamez.memories.struct.AbstractStruct;
 import fr.xamez.memories.utils.Pair;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.WorldBorder;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Slab;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.WeakHashMap;
 
 public class PlayerListener implements Listener {
+
+    private final WeakHashMap<Player, ArrayList<Location>> PLAYERS_BLOCKS = new WeakHashMap<>();
 
     @EventHandler
     public void playerJoin(PlayerJoinEvent e) {
         Memories.GAME.setupPlayer(e.getPlayer());
+        PLAYERS_BLOCKS.put(e.getPlayer(), new ArrayList<>());
         if (!Memories.GAME.canJoin())
             e.getPlayer().setGameMode(GameMode.SPECTATOR);
     }
@@ -54,12 +64,43 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onPlace(BlockPlaceEvent e){
-        if (!Memories.GAME.canBuild() && !e.getPlayer().isOp()) e.setCancelled(true);
+        if (!Memories.GAME.canBuild() && !e.getPlayer().isOp())
+            e.setCancelled(true);
+        else
+            PLAYERS_BLOCKS.get(e.getPlayer()).add(e.getBlock().getLocation());
     }
 
     @EventHandler
-    public void onPlace(BlockBreakEvent e){
-        if (!Memories.GAME.canBuild() && !e.getPlayer().isOp()) e.setCancelled(true);
+    public void onBreak(BlockBreakEvent e){
+        if (!e.getPlayer().isOp())
+            e.setCancelled(true);
+        else
+            PLAYERS_BLOCKS.get(e.getPlayer()).remove(e.getBlock().getLocation());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onLeftClick(PlayerInteractEvent e){
+        if (e.getPlayer().isOp()) return;
+        if (!Memories.GAME.canBuild()) return;
+        if (!e.getAction().equals(Action.LEFT_CLICK_BLOCK)) return;
+        if (!PLAYERS_BLOCKS.get(e.getPlayer()).contains(e.getClickedBlock().getLocation())) return;
+        final Block block = e.getClickedBlock();
+        final ItemStack itemStack = new ItemStack(block.getType());
+        if (block.getBlockData() instanceof Slab)
+            if (((Slab) block.getBlockData()).getType().equals(Slab.Type.DOUBLE)) // if the block is a double slab, we give 1 more
+                e.getPlayer().getInventory().addItem(itemStack);
+        e.getPlayer().getInventory().addItem(itemStack);
+        block.setType(Material.AIR);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e){
+        if (!e.getPlayer().isOp()) e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerEntityInteract(PlayerInteractEntityEvent e){
+        if (!e.getPlayer().isOp()) e.setCancelled(true);
     }
 
 }
